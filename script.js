da28879d6696b7442d0b3b62f4210b0697392ac9
Dayron Cruz <dayron10cruz@gmail.com>
2024-08-17 14:19:54 -0500

    update cript.js

--- script.js
+++ script.js
@@ -55,12 +55,13 @@ function desencriptar(stringDesencriptada) {
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringDesencriptada = stringDesencriptada.toLowerCase();
 
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
-
+    for (let i = 0; i < matrizCodigo.length; i++) {                
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+                stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+    
+            }
+    
         }
-
+        return stringDesencriptada
     }
-    return stringDesencriptada
-}
+    
